e7e9dc650aee9962f62a6664cb8405861eed0c9d
Code-Apprentice <codeguru42@gmail.com>
2023-12-20 20:29:54 -0700

    writeChunk() and supporting macros and functions

--- chunk.c
+++ chunk.c
@@ -1,9 +1,21 @@
 #include <stdlib.h>
 
 #include "chunks.h"
+#include "memory.h"
 
 void initChnk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+}
+
+void writeChnk(Chunk* chunk, uint8_t byte) {
+    if (chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    }
+
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
 }
