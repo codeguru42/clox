4c451832734b5436ff3ddf037ec5de7184d045b5
Code-Apprentice <codeguru42@gmail.com>
2023-12-22 20:26:00 -0700

    Fix function names

--- chunk.c
+++ chunk.c
@@ -3,7 +3,7 @@
 #include "chunk.h"
 #include "memory.h"
 
-void initChnk(Chunk* chunk) {
+void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
@@ -11,10 +11,10 @@ void initChnk(Chunk* chunk) {
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    initChnk(chunk);
+    initChunk(chunk);
 }
 
-void writeChnk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
