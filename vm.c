3fd16133740aca34f558b6f29669042481681258
Code-Apprentice <codeguru42@gmail.com>
2024-01-21 15:48:54 -0700

    Add execution tracing

--- vm.c
+++ vm.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
-
 
 VM vm;
 
@@ -19,6 +19,9 @@ static InterpretResult run() {
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
     for(;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleChunk(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
