9c9e7fcd3dd5defd022df62142a587c1a52d5752
Code-Apprentice <codeguru42@gmail.com>
2024-01-27 19:10:28 -0700

    Consume whitespace

--- scanner.c
+++ scanner.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 typedef struct {
-    const char* start;
-    const char* current;
+    const char *start;
+    const char *current;
     int line;
 } Scanner;
 
@@ -24,16 +24,16 @@ static Token makeToken(TokenType type) {
     Token token;
     token.type = type;
     token.start = scanner.start;
-    token.length = (int)(scanner.current - scanner.start);
+    token.length = (int) (scanner.current - scanner.start);
     token.line = scanner.line;
     return token;
 }
 
-static Token errorToken(const char * message) {
+static Token errorToken(const char *message) {
     Token token;
     token.type = TOKEN_ERROR;
     token.start = message;
-    token.length = (int)strlen(message);
+    token.length = (int) strlen(message);
     token.line = scanner.line;
     return token;
 }
@@ -50,7 +50,31 @@ static bool match(char expected) {
     return true;
 }
 
+static char peek() {
+    return *scanner.current;
+}
+
+static void skipWhitespace() {
+    for (;;) {
+        char c = peek();
+        switch (c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                advance();
+                break;
+            case '\n':
+                scanner.line++;
+                advance();
+                break;
+            default:
+                return;
+        }
+    }
+}
+
 Token scanToken() {
+    skipWhitespace();
     scanner.start = scanner.current;
 
     if (isAtEnd()) return makeToken(TOKEN_EOF);
