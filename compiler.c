d9f1a55c841eb4774be55a0761de7bfd66ba1f7b
Code-Apprentice <codeguru42@gmail.com>
2024-05-16 16:38:17 -0600

    Error handling

--- compiler.c
+++ compiler.c
@@ -7,9 +7,37 @@
 typedef struct {
     Token current;
     Token previous;
+    bool hadError;
+    bool panicMode;
 } Parser;
 
 Parser parser;
+
+static void errorAt(Token *token, const char *message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing.
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char *message) {
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char *message) {
+    errorAt(&parser.current, message);
+
+}
 
 static void advance() {
     parser.previous = parser.current;
@@ -23,8 +51,11 @@ static void advance() {
 }
 
 bool compile(const char *source, Chunk *chunk) {
+    parser.hadError = false;
+    parser.panicMode = false;
     initScanner(source);
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
