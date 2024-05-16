d32cd3809675e9b5746ea1011d410f7fb02df3b4
Code-Apprentice <codeguru42@gmail.com>
2024-05-16 16:01:01 -0600

    advance()

--- compiler.c
+++ compiler.c
@@ -4,6 +4,24 @@
 
 #include "scanner.h"
 
+typedef struct {
+    Token current;
+    Token previous;
+} Parser;
+
+Parser parser;
+
+static void advance() {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
 bool compile(const char *source, Chunk *chunk) {
     initScanner(source);
     advance();
