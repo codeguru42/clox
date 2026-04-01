62cef47046d66945880f14729c63a89908c5481d
codeguru42 <codeguru42@gmail.com>
2026-03-31 21:23:30 -0600

    getRule()

--- compiler.c
+++ compiler.c
@@ -210,6 +210,10 @@ static void parsePrecedence(Precedence precedence) {
 
 }
 
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
+}
+
 static  void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
